f98172750bcd335003836be3bec3ee18b59a0cb3
Manidoux41 <lherissondu41@gmail.com>
2023-11-28 08:54:21 +0100

    change language

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 const API_KEY = "api_key=03039258647674b32eb550684034fbf3";
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+const API_LANG = "language=fr-FR";
+const API_URL = `${BASE_URL}/discover/movie?${API_LANG}&sort_by=popularity.desc&${API_KEY}`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const searchURL = `${BASE_URL}/search/movie?${API_KEY}&query=`;
 
